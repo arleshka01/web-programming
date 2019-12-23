273170a6a58573b81ffb891cb2a1d486fcfd3850
arleshka01 <55532010+arleshka01@users.noreply.github.com>
2019-12-24 00:01:46 +0600

    Create about_us.php

---
+++ final/about_us.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arlen Pants</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="script.js"></script>
+	<style type="text/css">
+	</style>
+</head>
+
+<body>
+	<header>
+		<div>
+			<a href="" class="header_menu">
+				English
+			</a>
+			<a href="" class="header_menu">
+				USD
+			</a>
+		</div>
+		<div>
+			<a href="index.php" class="header_menu">
+				Home
+			</a>
+
+			<a href="" class="header_menu">
+				Help
+			</a>
+			<a href="about_us.php" class="header_menu">
+				About Us
+			</a>
+
+			<a href="" class="header_menu">
+				Wish List
+			</a>
+			<a href="register.php" class="header_menu">
+				My Account
+			</a>
+		</div>
+
+	</header>
+
+
+
+	<div id="about_us">
+		<h1>About us</h1>
+		<p>Arlen Pants is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook..Arlen Pants was founded by Jeff Bezos in Bellevue, Washington, in July 2019. The company initially started as an online marketplace for books but later expanded to sell electronics, software, video games, apparel, furniture, food, toys, and jewelry. In 2019, Arlen Pants surpassed Walmart as the most valuable retailer in the United States by market capitalization.[18] In 2017, Amazon acquired Whole Foods Market for US$13.4 billion, which vastly increased Amazon's presence as a brick-and-mortar retailer.[19] In 2018, Bezos announced that its two-day delivery service, Amazon Prime, had surpassed 100 million subscribers worldwide.[20][21]</p>
+
+	</div>
+
+
+    <footer class="footer">
+        <div class="row">
+          <div class="footer_about">
+            <h6>About</h6>
+            <p class="text-justify">Arlen Pants is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook..</p>
+          </div>
+
+          <div class="footer_category">
+            <h6>Categories</h6>
+            <ul class="footer_links">
+              <li><a href="#">Jeans</a></li>
+              <li><a href="categoty.php">Sport</a></li>
+              <li><a href="#">Casual</a></li>
+              <li><a href="#">Classical</a></li>
+              <li><a href="#">Jogger</a></li>
+              
+            </ul>
+          </div>
+
+          <div class="footer_link">
+            <h6>Quick Links</h6>
+            <ul class="footer_links">
+              <li><a href="about_us.php">About Us</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Our Address</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Other Indormation</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer_line">
+        	<hr>
+        </div>
+    
+    
+          <div class="footer_copyright">
+            <p class="copyright_text">Copyright &copy; 2019 All Rights Reserved by 
+         	<a href="about_us.php">Arlen Pants</a>.
+            </p>
+          </div>
+</footer>
+
+</body>
+</html>
+
+
+
