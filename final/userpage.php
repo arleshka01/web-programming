fa39885aac0635030023f5b3a219b2f359ee7624
arleshka01 <55532010+arleshka01@users.noreply.github.com>
2019-12-24 00:04:34 +0600

    Create userpage.php

---
+++ final/userpage.php
@@ -0,0 +1,207 @@
+<?php include('server.php'); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Arlen Pants</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="script.js"></script>
+</head>
+
+<body>
+	<header>
+		<div>
+			<a href="" class="header_menu">
+				English
+			</a>
+			<a href="" class="header_menu">
+				USD
+			</a>
+		</div>
+		<div>
+			<a href="index.php" class="header_menu">
+				Home
+			</a>
+
+			<a href="" class="header_menu">
+				Help
+			</a>
+			<a href="about_us.php" class="header_menu">
+				About Us
+			</a>
+
+			<a href="" class="header_menu">
+				Wish List
+			</a>
+			<a href="#" class="header_menu">
+				<?php if(isset($_SESSION['username'])): ?>
+					Welcome <?php echo $_SESSION['username']; ?>
+					<?php endif ?>
+			</a>
+			<a href="index.php" class="header_menu">
+				Logout
+			</a>
+		</div>
+
+	</header>
+
+
+	<section class="finder">
+		<img src="logo.png" id="logo">
+		<div id="search">
+			<input type="text" name="" id="find_input" placeholder="Enter keywords here...">
+			<a id="button_all_categories">All Categories</a>
+			<a href="#" id="search_button"><img src="search.png" id="search_image"></a>
+		</div>
+		<div id="shopping_cart">
+			<img id="cart" src="cart.png">
+			<div id="shopping_cart_info">
+				<a>Shopping Cart<br>0 item - 0.00$</a>
+			</div>
+		</div>
+	</section>
+
+	<div id ="image">
+				<img id="mainImg">
+	</div>
+
+	<div class="medium">
+		<div id="main_menu">	
+	        <a class="main_menu_items" href="#">Jeans</a>
+			<a class="main_menu_items" href="category.php">Sport</a>
+	  		<a class="main_menu_items" href="#">Casual</a>
+			<a class="main_menu_items" href="#">Classical</a>
+			<a class="main_menu_items" href="#">Joggers</a>
+		</div>
+	</div>
+
+
+
+	
+	<section class="section_products">
+		<div class="products_list_q">
+			<div class="products_shell">
+				<img class="image_product" src="pants1.jpeg">
+				<a class="name_product" href="product_page.html">Name of Product 1</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="pants2.jpg">
+				<a class="name_product" href="">Name of Product 2</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="pants3.jpg">
+				<a class="name_product" href="">Name of Product 3</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="pants4.jpeg">
+				<a class="name_product" href="">Name of Product 4</a>
+				<p>$39.00</p>
+			</div>
+		
+			
+		
+			<div class="products_shell">
+				<img class="image_product" src="casual1.jpeg">
+				<a class="name_product" href="">Name of Product 1</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="casual2.jpg">
+				<a class="name_product" href="">Name of Product 2</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="casual3.jpeg">
+				<a class="name_product" href="">Name of Product 3</a>
+				<p>$39.00</p>
+			</div>
+
+
+			<div class="products_shell">
+				<img class="image_product" src="casual4.jpeg">
+				<a class="name_product" href="">Name of Product 4</a>
+				<p>$39.00</p>
+			</div>
+		
+			
+		</div>
+
+
+
+	</section>
+
+
+  
+
+    <footer class="footer">
+        <div class="row">
+          <div class="footer_about">
+            <h6>About</h6>
+            <p class="text-justify">Arlen Pants is an American multinational technology company based in Seattle that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence. It is considered one of the Big Four tech companies, along with Google, Apple, and Facebook..</p>
+          </div>
+
+          <div class="footer_category">
+            <h6>Categories</h6>
+            <ul class="footer_links">
+              <li><a href="#">Jeans</a></li>
+              <li><a href="categoty.php">Sport</a></li>
+              <li><a href="#">Casual</a></li>
+              <li><a href="#">Classical</a></li>
+              <li><a href="#">Jogger</a></li>
+              
+            </ul>
+          </div>
+
+          <div class="footer_link">
+            <h6>Quick Links</h6>
+            <ul class="footer_links">
+              <li><a href="about_us.php">About Us</a></li>
+              <li><a href="#">Contact Us</a></li>
+              <li><a href="#">Our Address</a></li>
+              <li><a href="#">Privacy Policy</a></li>
+              <li><a href="#">Other Indormation</a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="footer_line">
+        	<hr>
+        </div>
+    
+    
+          <div class="footer_copyright">
+            <p class="copyright_text">Copyright &copy; 2019 All Rights Reserved by 
+         	<a href="about_us.html">Arlen Pants</a>.
+            </p>
+          </div>
+</footer>
+<script type="text/javascript">
+	document.getElementById('shopping_cart').style.border = '1px solid black';
+	fetch('http://localhost:8080/registration/image.json')
+.then(response => response.json())
+.then(data => {
+    var divImage = document.getElementById('image');
+    divImage.innerHTML = data.image;
+    var mainImg = document.getElementById('mainImg');
+    
+
+
+})
+.catch(error => console.error(error))
+
+
+</script>
+
+</body>
+</html>
